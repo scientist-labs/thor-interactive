ab42fb92b001ce1c887d77baaed0f6a08a74d7d7
Chris Petersen <chris@petersen.io>
2025-09-06 11:32:22 -0700

    Initial commit

---
+++ lib/thor/interactive.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "interactive/version"
+
+module Thor
+  module Interactive
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/thor/interactive/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Thor
+  module Interactive
+    VERSION = "0.1.0"
+  end
+end
